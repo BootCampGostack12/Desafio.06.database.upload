3bded80a0909330be250f72c8720e5c2e0753bd1
mpgxc <mpgx5.c@gmail.com>
2020-08-30 02:32:18 -0300

    fix: remove invalid column

--- src/services/CreateTransactionService.ts
+++ src/services/CreateTransactionService.ts
@@ -1,11 +1,51 @@
-// import AppError from '../errors/AppError';
-
+import { getRepository } from 'typeorm';
+import AppError from '../errors/AppError';
 import Transaction from '../models/Transaction';
+import Category from '../models/Category';
+
+interface RequestDTO {
+    title: string;
+    value: number;
+    type: string;
+    category: string;
+}
 
 class CreateTransactionService {
-  public async execute(): Promise<Transaction> {
-    // TODO
-  }
+    public async execute({
+        title,
+        value,
+        type,
+        category,
+    }: RequestDTO): Promise<Transaction> {
+        const transactionRepository = getRepository(Transaction);
+        const categoryRepository = getRepository(Category);
+
+        const transactionExists = await transactionRepository.findOne({
+            where: { title },
+        });
+
+        const categoryExists = await categoryRepository.findOne({
+            where: { title: category },
+        });
+
+        // Caso não exista
+        if (!transactionExists || !categoryExists) {
+            let categoryObj = categoryRepository.create({
+                title: category,
+            });
+
+            categoryObj = await categoryRepository.save(categoryObj);
+
+            const transactionObj = transactionRepository.create({
+                title,
+                value,
+                type: 'income',
+                category_id: categoryObj.id,
+            });
+
+            await transactionRepository.save(transactionObj);
+        }
+    }
 }
 
 export default CreateTransactionService;
